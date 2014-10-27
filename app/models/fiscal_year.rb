876057c32186c7d0a671b2dba910b83863036646
mconiaris <mconiaris@gmail.com>
2014-10-27 16:43:26 -0400

    Added validations to FiscalYear model

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -1,2 +1,12 @@
 class FiscalYear < ActiveRecord::Base
+  validates :fiscal_year, presence: true, uniqueness: true
+  validates :receipts_current_dollars, presence: true
+  validates :outlays_current_dollars, presence: true
+  validates :surplus_deficit_current_dollars, presence: true
+  validates :receipts_constant, presence: true
+  validates :outlays_constant, presence: true
+  validates :surplus_deficit_constant, presence: true
+  validates :gdp_receipts, presence: true
+  validates :gdp_outlays, presence: true
+  validates :gdp_surplus_deficit, presence: true
 end
