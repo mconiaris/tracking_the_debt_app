65fae9df90cc922920e6ee3e59756040a679bde0
mconiaris <mconiaris@gmail.com>
2014-10-28 11:06:02 -0400

    Trying to get view set up.

--- app/helpers/fiscal_years_helper.rb
+++ app/helpers/fiscal_years_helper.rb
@@ -1,3 +1,9 @@
 module FiscalYearsHelper
 
+  def data_to_csv(data)
+    @csv_line = data.first.fiscal_year.to_s + "," +
+      data.first.receipts_current_dollars.to_s + "," +
+      data.first.outlays_current_dollars.to_s + "\n"
+  end
+
 end
