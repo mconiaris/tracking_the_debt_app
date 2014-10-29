ff432485f122f6618a72bc954d5b8b6b5490163b
mconiaris <mconiaris@gmail.com>
2014-10-29 16:41:52 -0400

    Added if statements to hide create, destroy, etc.

--- app/controllers/fiscal_years_controller.rb
+++ app/controllers/fiscal_years_controller.rb
@@ -14,50 +14,72 @@ class FiscalYearsController < ApplicationController
 
   # GET /fiscal_years/new
   def new
-    @fiscal_year = FiscalYear.new
+    if session[:access_token]
+      @fiscal_year = FiscalYear.new
+    else
+      redirect_to '/'
+    end
   end
 
   # GET /fiscal_years/1/edit
   def edit
+    if session[:access_token]
+    else
+      redirect_to '/'
+    end
   end
 
   # POST /fiscal_years
   # POST /fiscal_years.json
   def create
+    if session[:access_token]
+      respond_to do |format|
+        if @fiscal_year.save
+          format.html { redirect_to @fiscal_year, notice: 'Fiscal year was successfully created.' }
+          format.json { render :show, status: :created, location: @fiscal_year }
+        else
+          format.html { render :new }
+          format.json { render json: @fiscal_year.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      redirect_to '/'
+    end
+
     @fiscal_year = FiscalYear.new(fiscal_year_params)
 
-    respond_to do |format|
-      if @fiscal_year.save
-        format.html { redirect_to @fiscal_year, notice: 'Fiscal year was successfully created.' }
-        format.json { render :show, status: :created, location: @fiscal_year }
-      else
-        format.html { render :new }
-        format.json { render json: @fiscal_year.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /fiscal_years/1
   # PATCH/PUT /fiscal_years/1.json
   def update
-    respond_to do |format|
-      if @fiscal_year.update(fiscal_year_params)
-        format.html { redirect_to @fiscal_year, notice: 'Fiscal year was successfully updated.' }
-        format.json { render :show, status: :ok, location: @fiscal_year }
-      else
-        format.html { render :edit }
-        format.json { render json: @fiscal_year.errors, status: :unprocessable_entity }
+    if session[:access_token]
+      respond_to do |format|
+        if @fiscal_year.update(fiscal_year_params)
+          format.html { redirect_to @fiscal_year, notice: 'Fiscal year was successfully updated.' }
+          format.json { render :show, status: :ok, location: @fiscal_year }
+        else
+          format.html { render :edit }
+          format.json { render json: @fiscal_year.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to '/'
     end
+
   end
 
   # DELETE /fiscal_years/1
   # DELETE /fiscal_years/1.json
   def destroy
-    @fiscal_year.destroy
-    respond_to do |format|
-      format.html { redirect_to fiscal_years_url, notice: 'Fiscal year was successfully destroyed.' }
-      format.json { head :no_content }
+    if session[:access_token]
+      @fiscal_year.destroy
+      respond_to do |format|
+        format.html { redirect_to fiscal_years_url, notice: 'Fiscal year was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to '/'
     end
   end
 
