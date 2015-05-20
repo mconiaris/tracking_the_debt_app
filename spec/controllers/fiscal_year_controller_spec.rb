3ebb0625cf43e8c10bad4b246116f11302dd06fa
mconiaris <mconiaris@gmail.com>
2015-05-20 10:59:34 -0400

    Create test for about controller

--- spec/controllers/fiscal_year_controller_spec.rb
+++ spec/controllers/fiscal_year_controller_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe FiscalYearsController, :type => :controller do
-  it "renders the index template" do
-    get :index
-    expect(response).to render_template("index")
+  describe "index page" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+  describe "home page" do
+    it "renders the home template" do
+      get :home
+      expect(response).to render_template("home")
+    end
+  end
+  describe "about page" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
   end
 end
