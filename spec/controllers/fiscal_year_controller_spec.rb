bdf50ecdab98d62d86417a32bb0096cb8d2735d3
mconiaris <mconiaris@gmail.com>
2015-05-20 10:51:29 -0400

    Created test to confirm load of index

--- spec/controllers/fiscal_year_controller_spec.rb
+++ spec/controllers/fiscal_year_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe FiscalYearsController, :type => :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders the index template" do
+    get :index
+    expect(response).to render_template("index")
+  end
 end
