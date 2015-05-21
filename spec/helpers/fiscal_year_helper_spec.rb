51773eac15e7cbe6012bf5eccdd4bf0568fcd2a7
mconiaris <mconiaris@gmail.com>
2015-05-21 10:44:27 -0400

    Created tests for #constant_dollars helper

--- spec/helpers/fiscal_year_helper_spec.rb
+++ spec/helpers/fiscal_year_helper_spec.rb
@@ -21,6 +21,36 @@ RSpec.describe FiscalYearsHelper, :type => :helper do
     it "current_dollars[0].size returns 3" do
       expect(helper.current_dollars[0].size).to eq(3)
     end
-      # binding.pry
+    it "current_dollars[0][0] returns 1940" do
+      expect(helper.current_dollars[0][0]).to eq(1940)
+    end
+    it "current_dollars[10][1] returns 39.4" do
+      expect(helper.current_dollars[10][1]).to eq(39.4)
+    end
+    it "current_dollars[20][2] returns 92.2" do
+      expect(helper.current_dollars[20][2]).to eq(92.2)
+    end
   end
+
+  describe "#constant_dollars" do
+    it "returns an array" do
+      expect(helper.constant_dollars.class).to eq(Array)
+    end
+    it "array's size is 80" do
+      expect(helper.constant_dollars.size).to eq(80)
+    end
+    it "constant_dollars[1].size returns 3" do
+      expect(helper.constant_dollars[0].size).to eq(3)
+    end
+    it "constant_dollars[10][0] returns 1950" do
+      expect(helper.constant_dollars[10][0]).to eq(1950)
+    end
+    it "constant_dollars[11][1] returns 39.4" do
+      expect(helper.constant_dollars[11][1]).to eq(513.1)
+    end
+    it "constant_dollars[21][2] returns 92.2" do
+      expect(helper.constant_dollars[21][2]).to eq(678.2)
+    end
+  end
+      # binding.pry
 end
