219369b4728360e4dede77d689af154b61afe478
mconiaris <mconiaris@gmail.com>
2015-05-21 11:12:30 -0400

    Moved full_title method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Changes title of page
+  def full_title(page_title = '')
+    base_title = "Tracking the Debt"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
