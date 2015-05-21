2ba2af12ebc338d613ec2e5b5ef6cbf5cf93a874
mconiaris <mconiaris@gmail.com>
2015-05-21 12:54:51 -0400

    Created #full_title test when passing 'Home'

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,8 +2,11 @@ require "rails_helper"
 
 describe ApplicationHelper do
   describe "#full_title" do
-    it "returns the default title" do
+    it "returns 'Tracking the Debt' as the default title" do
       expect(helper.full_title).to eq("Tracking the Debt")
+    end
+    it "returns 'Home | Tracking the Debt' when it is passed 'Home'" do
+      expect(helper.full_title('Home')).to eq("Home | Tracking the Debt")
     end
   end
 end
