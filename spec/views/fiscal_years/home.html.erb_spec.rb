1e67ea0d57d5fd261bf526304b44714dc9f996fc
mconiaris <mconiaris@gmail.com>
2015-05-21 08:52:07 -0400

    Created view test.

--- spec/views/fiscal_years/home.html.erb_spec.rb
+++ spec/views/fiscal_years/home.html.erb_spec.rb
@@ -2,14 +2,25 @@ require 'rails_helper'
 
 # TODO: Un-comment tests to research & make pass
 RSpec.describe "fiscal_years/home", :type => :view do
-  #   before(:each) do
-  #   assign(:fiscal_years, [
-  #     FiscalYear.create!(),
-  #     FiscalYear.create!()
-  #   ])
-  # end
 
-  # it "renders a list of fiscal_years" do
-  #   render
-  # end
+  before(:each) do
+    assign(:fiscal_years, [
+      FiscalYear.create!({
+        :fiscal_year => 2012,
+        :receipts_current_dollars => 2450.2,
+        :outlays_current_dollars => 3537.1,
+        :surplus_deficit_current_dollars => -1087.0,
+        :receipts_constant => 2331.0,
+        :outlays_constant => 3365.2,
+        :surplus_deficit_constant => -1034.1,
+        :gdp_receipts => 15.2,
+        :gdp_outlays => 22.0,
+        :gdp_surplus_deficit => -6.8
+      })
+    ])
+  end
+
+  it "renders a list of fiscal_years" do
+    render
+  end
 end
