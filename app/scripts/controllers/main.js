ab062fab50944b29ce7bfd2be3df5608ffd6898b
mconiaris <mconiaris@gmail.com>
2015-10-06 11:12:19 -0400

    Ran yo angular to install angular and Bootstrap files.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trackingTheDebtAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trackingTheDebtAppApp
+ */
+angular.module('trackingTheDebtAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
