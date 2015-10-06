8926e8af50d2f441da9721cd4e1b1a454a0826df
mconiaris <mconiaris@gmail.com>
2015-10-06 09:27:43 -0400

    Modified gruntfile for http-server

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,49 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint', 'qunit']
+    },
+    'http-server': {
+
+      'dev': {
+        // the server root directory
+        root: <path>,
+
+        // the server port
+        // can also be written as a function, e.g.
+        // port: function() { return 8282; }
+        port: 8282,
+
+        // the host ip address
+        // If specified to, for example, "127.0.0.1" the server will
+        // only be available on that ip.
+        // Specify "0.0.0.0" to be available everywhere
+        host: "0.0.0.0",
+
+        cache: <sec>,
+        showDir : true,
+        autoIndex: true,
+
+        // server default file extension
+        ext: "html",
+
+        // run in parallel with other tasks
+        runInBackground: true|false,
+
+        // specify a logger function. By default the requests are
+        // sent to stdout.
+        logFn: function(req, res, error) { },
+
+        // Proxies all requests which can't be resolved locally to the given url
+        // Note this this will disable 'showDir'
+        proxy: "http://someurl.com",
+
+        /// Use 'https: true' for default module SSL configuration
+        /// (default state is disabled)
+        https: {
+            cert: "cert.pem",
+            key : "key.pem"
+        }
+      }
     }
   });
 
@@ -53,6 +96,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-http-server');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
 
