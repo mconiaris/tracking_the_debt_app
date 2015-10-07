b5b529d95580397b48a830e5cc60868d3f486aad
mconiaris <mconiaris@gmail.com>
2015-10-07 11:29:10 -0400

    Deleted original config for grunt-open

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,19 +188,6 @@ module.exports = function (grunt) {
       }
     },
 
-      // build : {
-      //   path : 'http://google.com/',
-      //   app: 'Firefox'
-      // },
-      // file : {
-      //   path : '/etc/hosts'
-      // },
-      custom: {
-        path : function () {
-          return grunt.option('path');
-        }
-      }
-    },
 
     // Automatically inject Bower components into the app
     wiredep: {
