22d0fac718303df011fa2526ca2d03e31dbe8e98
mconiaris <mconiaris@gmail.com>
2015-10-10 09:15:30 -0400

    Testing  functionality.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,25 @@
  * Controller of the trackingTheDebtAppApp
  */
 angular.module('trackingTheDebtApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     // Testing this out to see if I can retrieve this value
     $scope.page = 'home';
     // TODO: Put Dygraphs here?
     console.log($scope.page);
+
+    $http.get('scripts/graph.js').success(function(data) {
+    $scope.graph = data;
   });
 
+  // $scope.orderProp = 'age';
+  console.log($scope.graph)
+  });
+
+
+// phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+//   $http.get('phones/phones.json').success(function(data) {
+//     $scope.phones = data;
+//   });
+
+//   $scope.orderProp = 'age';
+// });
