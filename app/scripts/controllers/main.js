6d11f87ab717c30ed4fa72f33326ba70306f9229
mconiaris <mconiaris@gmail.com>
2015-10-20 11:03:51 -0400

    Added currentDollars method again and put in a console.log.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -112,6 +112,9 @@ myApp.controller('GraphCtrl', ['$scope', function($scope) {
     }
   };
   console.log($scope.graph);
+  $scope.currentDollars = function() {
+    console.log("currentDollars button pressed.");
+  };
 }]);
 
 
