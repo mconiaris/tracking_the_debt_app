d4057a647f773dbc659e5076cc2e2ab96de14b44
mconiaris <mconiaris@gmail.com>
2015-11-16 12:14:18 -0500

    Added grunt-boostrap to Grunfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -427,6 +427,8 @@ module.exports = function (grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-bootstrap');
+
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
