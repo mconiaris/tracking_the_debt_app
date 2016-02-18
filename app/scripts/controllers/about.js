f2e8c6f296ddfdbe14d12c8b3d84e2d43c317969
mconiaris <mconiaris@gmail.com>
2016-02-18 14:41:07 -0500

    Yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trackingTheDebtAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trackingTheDebtAppApp
+ */
+angular.module('trackingTheDebtAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
