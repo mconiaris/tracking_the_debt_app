ff8a79b4f453afc5f3baa8c72430a4786f440019
mconiaris <mconiaris@gmail.com>
2016-02-18 13:11:19 -0500

    Added anote of older configuration in web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express'); // plugin for express, a server framework
 var morgan = require('morgan'); // HTTP request logger middleware for node.js
 var app = express();
 
+// app.use(morgan('dev'));
 app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
