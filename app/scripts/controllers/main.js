3c2ff8f8784b8b920a174e9fa8e44f933fb42744
mconiaris <mconiaris@gmail.com>
2016-03-01 09:40:42 -0500

    Added template text and data to graph. Graph is not loading.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,4 +22,27 @@ myApp.controller('GraphCtrl', ['$scope', function($scope) {
   $scope.message = 'GraphCtrl loaded.';
   console.log($scope.message);
 
+  $scope.graph = {
+    data: [
+      [1940,6.5,9.5],
+      [1941,8.7,13.7],
+      [1942,14.6,35.1],
+    ],
+    options: {
+      labels: ['x', 'A', 'B']
+    },
+    legend: {
+      series: {
+        A: {
+          label: 'Series A'
+        },
+        B: {
+          label: 'Series B',
+          format: 3
+        }
+      }
+    }
+  };
+
+
 }]);
