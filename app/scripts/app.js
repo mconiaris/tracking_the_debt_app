d309236cd55d2dff37da19919d7e016269372267
mconiaris <mconiaris@gmail.com>
2016-03-11 15:42:58 -0500

    Removed angular-dygraphs reference from app.js module list.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,7 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch',
-    'angular-dygraphs'
+    'ngTouch'
   ])
   .config(function ($routeProvider) {
     $routeProvider
