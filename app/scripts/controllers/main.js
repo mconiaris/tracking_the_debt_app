ddf2da34b6f977e71e5b15d574e0a645adcb8525
mconiaris <mconiaris@gmail.com>
2016-03-11 16:29:04 -0500

    Modified template in dygraph directive.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -126,9 +126,12 @@ myApp.directive('dygraphs', function() {
     restrict: 'E', // Use as element
     scope: { // Isolate scope
         data: '=', // Two-way bind data to local scope
-        opts: '=?' // '?' means optional
+        options: '=?' // '?' means optional
     },
-    template: "<div id=\"graph\"></div>", // We need a div to attach graph to
+    template: "<div id=\"graph\"></div>" +
+    "<dygraphs ng-if=\"graph.data.length\" data=\"graph.data\" options=\"graph.options\" legend=\"graph.legend\" >" +
+    '<p>Inside dygraph</p>' +
+  '</dygraphs>', // We need a div to attach graph to
     link: function(scope, elem, attrs) {
 
       var graph = new Dygraph(elem.children()[0], scope.data, scope.opts );
