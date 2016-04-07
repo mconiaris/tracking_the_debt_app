06729daf5dc39ff61d2f81be88c4b0e2eced165f
mconiaris <mconiaris@gmail.com>
2016-04-07 11:05:55 -0400

    Added notes relating to controllers.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,10 @@
 // functions" that are responsible for providing
 // the application behavior that supports the
 // declarative markup in the template.
+// Controllers are associated with an element
+// in the DOM and so are provided with access
+// to the scope. Other components (like services)
+// only have access to the $rootScope service.
 var myApp = angular.module('trackingTheDebtAppApp')
   .controller('MainCtrl', function () {
     this.awesomeThings = [
