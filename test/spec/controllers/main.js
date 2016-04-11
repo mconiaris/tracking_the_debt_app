10edd40dc84a572ba186ba9b62dff9f27e206475
mconiaris <mconiaris@gmail.com>
2016-04-11 11:38:31 -0400

    Added test for GraphCtrl console log

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -22,3 +22,25 @@ describe('Controller: MainCtrl', function () {
   });
 });
 
+
+describe('Controller: GraphCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('trackingTheDebtAppApp'));
+
+  var GraphCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    GraphCtrl = $controller('GraphCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('should attach a message reading \'GraphCtrl loaded\' to the scope', function () {
+    expect(MainCtrl.message).toBe('GraphCtrl loaded');
+  });
+});
