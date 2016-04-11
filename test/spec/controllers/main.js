123f40daf7804c49393cdfde31819cb12ed45f5b
mconiaris <mconiaris@gmail.com>
2016-04-11 11:35:40 -0400

    Deleted test for controller was was removed.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -22,24 +22,3 @@ describe('Controller: MainCtrl', function () {
   });
 });
 
-describe('Controller: DropdownCtrl', function() {
-
-  // load the controller's module
-  beforeEach(module('trackingTheDebtAppApp'));
-
-  var DropdownCtrl, scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    DropdownCtrl = $controller('DropdownCtrl', {
-      $scope: scope
-      // place here mocked dependencies
-    });
-  }));
-
-  it('should attach a message reading \'DropdownCtrl loaded\' to the scope', function () {
-    expect(DropdownCtrl.message).toBe('DropdownCtrl loaded');
-  });
-
-});
