ce591755b1c3ccd604d3ab4c635cb8cf1258a52e
mconiaris <mconiaris@gmail.com>
2016-04-18 11:38:01 -0400

    Created test to confirm array item is not null.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -119,6 +119,9 @@ describe('Controller: GraphCtrl', function () {
 
 
   describe('scope.graphs[0].data[0][1]', function() {
+    it('should not be null', function() {
+      expect(scope.graphs[0].data[0][1]).toEqual(jasmine.anything());
+    });
     it('should be a Number', function () {
       expect(scope.graphs[0].data[0][1]).toEqual(jasmine.any(Number));
     });
