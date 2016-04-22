1a5d2d56fafa984f6dd3ab576a623515d1166c34
mconiaris <mconiaris@gmail.com>
2016-04-22 18:45:23 -0400

    Deleted template array from AboutCtrl.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,9 +9,5 @@
  */
 angular.module('trackingTheDebtAppApp')
   .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
