a7d4ec2d9b966441cd1d3bf692da2e65352c5bd0
mconiaris <mconiaris@gmail.com>
2016-04-22 19:14:34 -0400

    Created a variable for myApp in graph.js controller.

--- app/scripts/controllers/graph.js
+++ app/scripts/controllers/graph.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('trackingTheDebtAppApp')
+var myApp = angular.module('trackingTheDebtAppApp')
   .controller('GraphCtrl', function () {
     this.message = 'GraphCtrl loaded.';
   });
