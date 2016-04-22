2fcfe4776b22593503a1fc1656fa7960cee70fc2
mconiaris <mconiaris@gmail.com>
2016-04-22 18:22:08 -0400

    Deleted redundant test.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -331,15 +331,6 @@ describe('Controller: GraphCtrl', function () {
 
 
   // scope.graphs[0].data[60]
-  describe('scope.graphs[0].data[60][0]', function() {
-    it('should not be null', function() {
-      expect(scope.graphs[0].data[60][0]).toEqual(jasmine.anything());
-    });
-    it('should be a Number', function () {
-      expect(scope.graphs[0].data[60][0]).toEqual(jasmine.any(Number));
-    });
-  });
-
   describe('scope.graphs[0].data[60][1]', function() {
     it('should not be null', function() {
       expect(scope.graphs[0].data[60][1]).toEqual(jasmine.anything());
