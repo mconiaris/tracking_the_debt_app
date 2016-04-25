7d006438636c794fc3f3d6d211b7fc6215b77cb1
mconiaris <mconiaris@gmail.com>
2016-04-25 12:23:34 -0400

    Completely removed GraphCtrl tests from main.js test file.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -28,31 +28,3 @@ describe('Controller: MainCtrl', function () {
     expect(MainCtrl.message).toBe('MainCtrl loaded');
   });
 });
-
-
-// A test suite begins with a call to the global
-// Jasmine function describe with two parameters:
-// a string and a function. The string is a name
-// or title for a spec suite – usually what is
-// being tested. The function is a block of code
-// that implements the suite.
-describe('Controller: GraphCtrl', function () {
-
-  // load the controller's module
-  // JavaScript scoping rules apply, so variables
-  // declared in a describe are available to any
-  // it block inside the suite.
-  beforeEach(module('trackingTheDebtAppApp'));
-
-  var GraphCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    GraphCtrl = $controller('GraphCtrl', {
-      $scope: scope
-      // place here mocked dependencies
-    });
-  }));
-});
