c524437c4d6446c81ad11fb5f7bb3ea2f4480480
mconiaris <mconiaris@gmail.com>
2016-04-25 11:44:24 -0400

    Added tests so that they dynamically check for not null and a Number data type.

--- test/spec/controllers/graph.js
+++ test/spec/controllers/graph.js
@@ -469,6 +469,12 @@ describe('Controller: GraphCtrl', function () {
     var arraySize = 80;
 
     function yearTest(graphIndex, year) {
+      it('in scope.graphs[1].data[0][' + graphIndex + '] should not be null', function() {
+        expect(scope.graphs[1].data[graphIndex][0]).toEqual(jasmine.anything());
+      });
+      it('in scope.graphs[1].data[0][' + graphIndex + '] should be a Number', function () {
+        expect(scope.graphs[1].data[graphIndex][0]).toEqual(jasmine.any(Number));
+      });
       it('in scope.graphs[1].data[0][' + graphIndex + '] should equal \'' + year + '\'', function () {
         expect(scope.graphs[1].data[graphIndex][0]).toBe(year);
       });
